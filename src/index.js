b776a058753317668e68896cb7561f2709001cf4
Gagan Tayal <gagantayal2003@gmail.com>
2023-05-02 23:05:52 +0530

    await updates

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ app.post('/user',async (req,res)=>{
     catch(e){
         res.status(400).send(e)  
     }
+
     // user.save().then(()=>{
     //     res.status(201).send(user)
     //     console.log(user)
@@ -26,7 +27,7 @@ app.post('/user',async (req,res)=>{
     // )
 })
 
-app.get('/user',(req,res)=>{
+app.get('/user', async(req,res)=>{
     try{
         const users = await User.find({})
         res.send(users) 
@@ -35,56 +36,91 @@ app.get('/user',(req,res)=>{
         res.status(500).send()
     }
 
-
     // User.find({}).then((users) => {
     //     res.send(users)
     // }).catch((e) => {
     //     res.status(500).send()
     // })
 })
-app.get('/user/:id',(req,res)=>{
+app.get('/user/:id',async(req,res)=>{
     const _id = req.params.id
-    console.log(_id)
-    User.findById(_id).then((user) => {
-        console.log(user)
+    try{
+        const user = await User.findById(_id)
         if(!user){
             return res.status(404).send()
-        }
-        res.send(user)
-    }).catch((e) => {
+            }
+            res.send(user)
+    }
+    catch(e){
         res.status(500).send()
-    })
+    }
+
+    // const _id = req.params.id
+    // console.log(_id)
+    // User.findById(_id).then((user) => {
+    //     console.log(user)
+    //     if(!user){
+    //         return res.status(404).send()
+    //     }
+    //     res.send(user)
+    // }).catch((e) => {
+    //     res.status(500).send()
+    // })
 })
 
-app.post('/task',(req,res)=>{
-    const task = new Task(req.body)
-
-    task.save().then(()=>{
+app.post('/task',async(req,res)=>{
+     const task = new Task(req.body)
+    try{
+        await task.save()
         res.status(201).send(task)
-        console.log(task)
-    }).catch((e)=>{
-        res.status(400).send(e)
-    })
-})
-app.get('/task',(req,res)=>{
-    Task.find({}).then((tasks)=>{
-        res.send(tasks)
-    }).catch((e)=>{
+    }
+    catch(e){
         res.status(500).send(e)
-    })
+    }
+    // const task = new Task(req.body)
+    // task.save().then(()=>{
+    //     res.status(201).send(task)
+    //     console.log(task)
+    // }).catch((e)=>{
+    //     res.status(400).send(e)
+    // })
 })
-app.get('/task/:id',(req,res)=>{
+app.get('/task',async(req,res)=>{
+    try{
+    const tasks = await Task.find({})
+    res.status(201).send(tasks)}
+    catch(e){
+        res.status(400).send()}
+    // Task.find({}).then((tasks)=>{
+    //     res.send(tasks)
+    // }).catch((e)=>{
+    //     res.status(500).send(e)
+    // })
+})
+app.get('/task/:id',async(req,res)=>{
     const _id = req.params.id
-    console.log(_id)
-    Task.findById(_id).then((task) => {
-        console.log(task)
+    try{
+        const task = await Task.findById(_id)
         if(!task){
             return res.status(404).send()
-        }
-        res.send(task)
-    }).catch((e) => {
-        res.status(500).send(e)
-    }) 
+             }
+           res.send(task)  
+    }
+    catch(e){
+        res.status(400).send()
+    }
+
+    // const _id = req.params.id
+    // console.log(_id)
+    // Task.findById(_id).then((task) => {
+    //     console.log(task)
+    //     if(!task){
+    //         return res.status(404).send()
+    //     }
+    //     res.send(task)
+    // }).catch((e) => {
+    //     res.status(500).send(e)
+    // }) 
 })
 app.listen(port,()=>{
     console.log('server is  up on port ' + port)
