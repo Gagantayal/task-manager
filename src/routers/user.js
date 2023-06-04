a119d7996bb110a92e23de3bfbbac7e73ef88060
Gagan Tayal <gagantayal2003@gmail.com>
2023-06-04 09:03:18 +0530

    logout all user

--- src/routers/user.js
+++ src/routers/user.js
@@ -32,6 +32,30 @@ router.post('/user/login',async (req,res)=>{
         res.status(404).send(e)
     }
 })
+
+router.post('/user/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+router.post('/user/logoutAll', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
 router.get('/user/me', auth, async (req, res) => {
     res.send(req.user)
 })
